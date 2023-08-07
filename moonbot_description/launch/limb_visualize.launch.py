71358ff510909c9447de0a197fedd34be7d28f3c
TharitSinsunthorn <tharit.sinsunthorn.r1@dc.tohoku.ac.jp>
2023-08-07 14:41:36 +0900

    add one limb test model

---
+++ moonbot_description/launch/limb_visualize.launch.py
@@ -0,0 +1,73 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+import xacro
+
+# this is the function launch  system will look for
+def generate_launch_description():
+
+    ####### DATA INPUT ##########
+    urdf_file = 'single.urdf'
+    package_description = "moonbot_description"
+
+    ####### DATA INPUT END ##########
+    print("Fetching URDF ==>")
+    robot_desc_path = os.path.join(
+        get_package_share_directory(package_description), 
+        "urdf", 
+        urdf_file
+    )
+
+    # doc = xacro.parse(open(robot_desc_path))
+    # xacro.process_doc(doc)
+    # robot_description_config = doc.toxml()
+    # robot_description = {'use_sim_time': True,'robot_description': robot_description_config}
+
+    # Robot State Publisher
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher_node',
+        emulate_tty=True,
+        parameters=[{'use_sim_time': True, 'robot_description': Command(['xacro ', robot_desc_path])}],
+        # parameters=[robot_description],  ## for xacro
+        output="screen"
+    )
+
+    # Joint State Publisher
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen'
+    )
+
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(
+        get_package_share_directory(package_description), 
+        'rviz', 
+        'urdf_vis.rviz'
+    )
+
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        name='rviz_node',
+        parameters=[{'use_sim_time': True}],
+        arguments=['-d', rviz_config_dir]
+    )
+
+
+    # create and return launch description object
+    return LaunchDescription(
+        [            
+            robot_state_publisher_node,
+            rviz_node,
+            # joint_state_publisher_node
+        ]
+    )
--- moonbot_gazebo/scripts/action_joint.py
+++ moonbot_gazebo/scripts/action_joint.py
@@ -9,7 +9,7 @@ from control_msgs.action import FollowJointTrajectory
 from trajectory_msgs.msg import JointTrajectoryPoint
 
 # ros2 action list -t
-# ros2 action info /joint_trajectory_controller/follow_joint_trajectory -t
+# ros2 action info /position_trajectory_controller/follow_joint_trajectory -t
 # ros2 interface show control_msgs/action/FolowJointTrajectory
 
 
@@ -29,21 +29,67 @@ class LimbActionClient(Node):
         joint_names = ["j_c1_lf", "j_c1_rf", "j_c1_lr", "j_c1_rr",
                        "j_thigh_lf", "j_thigh_rf", "j_thigh_lr", "j_thigh_rr",
                        "j_tibia_lf", "j_tibia_rf", "j_tibia_lr", "j_tibia_rr"]
-        # joint_names = ["j_c1_lf", "j_c1_rf"]
+        
+        sec = 1.0
 
 
         points = []
         point1 = JointTrajectoryPoint()
-        point1.positions = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        point1.positions = [0.0, 0.0, 0.0, 0.0, 
+                            0.0, 0.0, 0.0, 0.0, 
+                            0.0, 0.0, 0.0, 0.0]
 
         point2 = JointTrajectoryPoint()
-        point2.time_from_start = Duration(seconds=1, nanoseconds=0).to_msg()
-        point2.positions = [0.0, 0.0, -0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        point2.time_from_start = Duration(seconds=sec, nanoseconds=0).to_msg()
+        point2.positions = [0.0, 0.0, 0.25, 0.0, 
+                            0.0, 0.0, -0.5, 0.0, 
+                            0.0, 0.0, 0.0, 0.0]
 
+        point3 = JointTrajectoryPoint()
+        point3.time_from_start = Duration(seconds=2*sec, nanoseconds=0).to_msg()
+        point3.positions = [0.0, 0.0, 0.5, 0.0, 
+                            0.0, 0.0, 0.0, 0.0, 
+                            0.0, 0.0, 0.0, 0.0]
         
+        point4 = JointTrajectoryPoint()
+        point4.time_from_start = Duration(seconds=3*sec, nanoseconds=0).to_msg()
+        point4.positions = [0.0, 0.0, 0.5, -0.25, 
+                            0.0, 0.0, 0.0, -0.5, 
+                            0.0, 0.0, 0.0, 0.0]
+        
+        point5 = JointTrajectoryPoint()
+        point5.time_from_start = Duration(seconds=4*sec, nanoseconds=0).to_msg()
+        point5.positions = [0.25, 0.0, 0.5, -0.5, 
+                            -0.5, 0.0, 0.0, 0.0, 
+                            0.0, 0.0, 0.0, 0.0]
+        
+        point6 = JointTrajectoryPoint()
+        point6.time_from_start = Duration(seconds=5*sec, nanoseconds=0).to_msg()
+        point6.positions = [0.5, 0.0, -0.0, 0.0,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+                            0.0, 0.0, 0.0, 0.0, 
+                            0.0, 0.0, 0.0, 0.0]
+
+        point7 = JointTrajectoryPoint()
+        point7.time_from_start = Duration(seconds=6*sec, nanoseconds=0).to_msg()
+        point7.positions = [0.5, -0.25, -0.0, 0.0,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+                            0.0, -0.5, 0.0, 0.0, 
+                            0.0, 0.0, 0.0, 0.0]
+        
+        point8 = JointTrajectoryPoint()
+        point8.time_from_start = Duration(seconds=7*sec, nanoseconds=0).to_msg()
+        point8.positions = [0.5, -0.5, -0.0, 0.0,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+                            0.0, 0.0, 0.0, 0.0, 
+                            0.0, 0.0, 0.0, 0.0]
+        
+        point9 = JointTrajectoryPoint()
+        point9.time_from_start = Duration(seconds=8*sec, nanoseconds=0).to_msg()
+        point9.positions = [0.0, -0.0, -0.0, 0.0,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+                            0.0, 0.0, 0.0, 0.0, 
+                            0.0, 0.0, 0.0, 0.0]
+
         # points.append(point1)
         # points.append(point2)
-        for i in range(2):
+        for i in range(9):
             point_name = f"point{i+1}"  # Generate the variable name using f-string
             point = eval(point_name)
             points.append(point)
@@ -76,7 +122,7 @@ class LimbActionClient(Node):
 
     def feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
-        #self.get_logger().info('Received feedbackl:'+str(feedback))
+        # self.get_logger().info('Received feedbackl:'+str(feedback))
 
 
 def main(args=None):
